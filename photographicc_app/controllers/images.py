a8340f8c4a5cc4f88e8e9ae95bdd6dcea829878f
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2022-08-04 23:43:22 -0400

    image files can be uploaded to server
    errors to be fixed still

---
+++ photographicc_app/controllers/images.py
@@ -0,0 +1,54 @@
+from ast import Try
+from photographicc_app import app
+from flask import flash, render_template,redirect,request,session
+from photographicc_app.models.image import Image
+# from photographicc_app.models import album
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+site_title = 'Photographicc'
+
+# Upload
+@app.route('/image_upload')
+def image_upload():
+    if 'user_id' in session:
+        return render_template('image_upload.html', title = site_title)
+    else:
+        return redirect('/user_login')
+# *****************************************************************************
+# uploading images through form ***********************************************
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+#This needs to be updated for the machine it's used on
+UPLOAD_FOLDER = '/Users/shaneazania/Documents/GitHub/Photographicc/photographicc_app/static/img/image_uploads'
+ALLOWED_EXTENSIONS = { 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/image_upload_form', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+
+
+# END uploading images ***********************************************
+# ****************************************************************
