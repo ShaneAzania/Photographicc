f6d0e94e2da66ac57311bc50e59d077a79f0525d
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2022-08-09 02:58:19 -0400

    make back button return to previous page in history

--- photographicc_app/static/js/index.js
+++ photographicc_app/static/js/index.js
@@ -9,3 +9,9 @@ close_btn.addEventListener('click', e => {
     info_box.style.width = '0';
     info_box.style.padding = '0';
 });
+
+// image_view back_btn returns to previous page in history
+const back_btn = document.querySelector('#back_btn');
+back_btn.addEventListener('click', e => {
+    history.back()
+});
